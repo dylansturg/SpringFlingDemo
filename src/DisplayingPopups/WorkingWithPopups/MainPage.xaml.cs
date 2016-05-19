bf854aeab64fe004a338f733cea7ce9e976afa3c
JBretz <JBretz@allegient.com>
2016-05-19 16:43:28 -0400

    Fixed Navigation Issue so that we can navigation on DataPages

--- src/DisplayingPopups/WorkingWithPopups/MainPage.xaml.cs
+++ src/DisplayingPopups/WorkingWithPopups/MainPage.xaml.cs
@@ -8,6 +8,23 @@ namespace WorkingWithPopups
 		public MainPage ()
 		{
 			InitializeComponent ();
+
+			var alertPage = new AlertPage();
+			alertPage.Title = "Alerts";
+			alertPage.Icon = "csharp.png";
+			var actionSheetPage = new ActionSheetPage();
+			actionSheetPage.Title = "Action Sheets";
+			actionSheetPage.Icon = "csharp.png";
+			var navigationPage = new NavigationPage();
+			var sampleDataPage = new SampleDataPage();
+			navigationPage.PushAsync(sampleDataPage);
+			navigationPage.Title = "Data Page";
+			navigationPage.Icon = "csharp.png";
+
+
+			Children.Add(alertPage);
+			Children.Add(actionSheetPage);
+			Children.Add(navigationPage);
 		}
 	}
 }
