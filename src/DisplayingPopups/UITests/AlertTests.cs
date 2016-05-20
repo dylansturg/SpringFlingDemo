9d4ef64a7994525c90f3c798a09d6a31efd2719c
Dylan Sturgeon <dylansturg@gmail.com>
2016-05-20 08:31:13 -0400

    add some example tests

---
+++ src/DisplayingPopups/UITests/AlertTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITests
+{
+	[TestFixture(Platform.iOS)]
+	public class AlertTests
+	{
+		IApp app;
+		Platform platform;
+
+		public AlertTests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
+
+
+	}
+}
+
---
+++ src/DisplayingPopups/UITests/DataPageTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITests
+{
+	[TestFixture(Platform.iOS)]
+	public class DataPageTests
+	{
+		IApp app;
+		Platform platform;
+
+		public DataPageTests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
+
+
+	}
+}
+
